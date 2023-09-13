a08653d23da191131b09e0657dae7bd4d5caaa3c
Esenha Developer <83224545+snhdev@users.noreply.github.com>
2023-09-13 11:13:23 +0700

    Add files via upload

---
+++ helpers/base-url.js
@@ -0,0 +1,7 @@
+module.exports = {
+    baseUrl: 'https://otakudesu.wiki/',
+    completeAnime:'complete-anime/',
+    onGoingAnime:'ongoing-anime/',
+    schedule:'jadwal-rilis/',
+    genreList:'genre-list/'
+};
